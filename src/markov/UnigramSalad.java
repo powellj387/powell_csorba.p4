2f026284198665b57744137d0d0201ee3a845b01
powellj387 <powellj387@lynchburg.edu>
2023-11-28 18:58:57 -0500

    working on ms2

--- src/markov/UnigramSalad.java
+++ src/markov/UnigramSalad.java
@@ -1,15 +1,24 @@
 package markov;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class UnigramSalad {
-
+    private Map<String, List<String>> markovChain = new HashMap<>();
+    private ArrayList<String> sVertexSuccessors = new ArrayList<>();
+    private int randSeed;
+    
     public UnigramSalad(int seed){
-
+        randSeed = seed;
+        //put the first "special vertex in the map with an empty successor list
+        markovChain.put("s",sVertexSuccessors);
     }
-
-
 
     public void addOrderedTokens(java.util.Collection<java.lang.String> tokens) throws InvalidInputException{
         //for every token check to see if in graph
+
         //if not in graph add to graph then create edge from token to next one
     }
 
