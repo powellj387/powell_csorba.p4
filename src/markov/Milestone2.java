cf75f8a7b4fd057d79b76f7dce9e14bf744a8516
AlexCsorba <124453983+AlexCsorba@users.noreply.github.com>
2023-12-04 16:11:05 -0500

    trying stuff for ms2

--- src/markov/Milestone2.java
+++ src/markov/Milestone2.java
@@ -1,5 +1,43 @@
 package markov;
 
-public class Milestone2 {
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public abstract class Milestone2 {
+    public static void main(String[] args) {
+
+        if (args.length != 3) {
+            System.err.println("Usage: java markov.Milestone2 <corpus_path> <seed> <num_sentences>");
+            System.exit(1);
+        }
+
+        String corpusPath = args[0];
+        String seed = args[1];
+        int numSentences = Integer.parseInt(args[2]);
+        Path path = Paths.get(corpusPath);
+
+        try {
+            // Break the corpus into sentences
+            List<String> sentences = LexicalParser.breakFileIntoSentences(path);
+
+            // Initialize a random number generator with the seed
+            Random rng = new Random(Long.parseLong(seed));
+
+            // Generate the requested number of sentences
+            for (int i = 0; i < numSentences; i++) {
+                // Select a random sentence from the corpus
+                String sentence = sentences.get(rng.nextInt(sentences.size()));
+
+                // Print the sentence
+                System.out.println(sentence);
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error processing the corpus: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
--- src/markov/UnigramSalad.java
+++ src/markov/UnigramSalad.java
@@ -4,26 +4,60 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+
 
 public class UnigramSalad {
+    // Map to store the Markov chain
     private Map<String, List<String>> markovChain = new HashMap<>();
+    // List to store successors of the special vertex "s"
     private ArrayList<String> sVertexSuccessors = new ArrayList<>();
+    // Seed for random number generator
     private int randSeed;
-    
+    // Random number generator
+    private Random rand;
+
+    // Constructor
     public UnigramSalad(int seed){
         randSeed = seed;
-        //put the first "special vertex in the map with an empty successor list
+        // Initialize the random number generator with the seed
+        this.rand = new Random(randSeed);
+        // Put the first "special vertex in the map with an empty successor list
         markovChain.put("s",sVertexSuccessors);
     }
 
+    // Method to add ordered tokens to the Markov chain
     public void addOrderedTokens(java.util.Collection<java.lang.String> tokens) throws InvalidInputException{
-        //for every token check to see if in graph
-
-        //if not in graph add to graph then create edge from token to next one
+        // For every token check to see if in graph
+        // If not in graph add to graph then create edge from token to next one
+        String prevToken = "s";
+        for(String token : tokens){
+            if(!markovChain.containsKey(prevToken)){
+                markovChain.put(prevToken, new ArrayList<>());
+            }
+            markovChain.get(prevToken).add(token);
+            prevToken = token;
+        }
     }
 
+    // Method to generate a sentence using the Markov chain
     public java.lang.String generateSentence() throws InsufficientMarkovChainException{
-        //use lottery algo for randomness
-        //use dijkstra's algo for weighted traversal
+        // Use lottery algo for randomness
+        // Use Dijkstra's algo for weighted traversal
+        if(markovChain.isEmpty()){
+            throw new InsufficientMarkovChainException("Markov Chain is empty");
+        }
+        String currentToken = "s";
+        StringBuilder sentence = new StringBuilder();
+        while(markovChain.containsKey(currentToken)){
+            List<String> nextTokens = markovChain.get(currentToken);
+            if(nextTokens.isEmpty()){
+                break;
+            }
+            int nextTokenIndex = rand.nextInt(nextTokens.size());
+            currentToken = nextTokens.get(nextTokenIndex);
+            sentence.append(currentToken).append(" ");
+        }
+        return sentence.toString().trim();
     }
 }
