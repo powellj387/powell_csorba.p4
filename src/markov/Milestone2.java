98dfe929e29674940d7b45b77798ef5aa195302a
powellj387 <powellj387@lynchburg.edu>
2023-12-04 23:29:36 -0500

    fixed milestone2 file to run correctly, need to weight the tokens such that the correct sentences are written out.

--- src/markov/Milestone2.java
+++ src/markov/Milestone2.java
@@ -6,38 +6,40 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public abstract class Milestone2 {
+public class Milestone2 {
     public static void main(String[] args) {
 
         if (args.length != 3) {
-            System.err.println("Usage: java markov.Milestone2 <corpus_path> <seed> <num_sentences>");
+            System.err.println("Usage: java markov.Milestone2 <corpus_path> <seed> <num_sentences");
             System.exit(1);
         }
 
         String corpusPath = args[0];
-        String seed = args[1];
+        int seed = Integer.parseInt(args[1]);
         int numSentences = Integer.parseInt(args[2]);
         Path path = Paths.get(corpusPath);
+        UnigramSalad UniSalad = new UnigramSalad(seed);
 
         try {
             // Break the corpus into sentences
             List<String> sentences = LexicalParser.breakFileIntoSentences(path);
 
-            // Initialize a random number generator with the seed
-            Random rng = new Random(Long.parseLong(seed));
-
-            // Generate the requested number of sentences
-            for (int i = 0; i < numSentences; i++) {
-                // Select a random sentence from the corpus
-                String sentence = sentences.get(rng.nextInt(sentences.size()));
-
-                // Print the sentence
-                System.out.println(sentence);
+            //break each sentence into its tokens and add them to the UnigramSalad
+            ArrayList<String> uniqueTokens = new ArrayList<>();
+            for (String sentence : sentences) {
+                List<String> tokens = LexicalParser.tokenizeSentence(sentence);
+                UniSalad.addOrderedTokens(tokens);
             }
 
+            //print out necessary number of sentences
+            for(int i=0;i<numSentences;i++) {
+                System.out.println(UniSalad.generateSentence());
+            }
         } catch (Exception e) {
             System.err.println("Error processing the corpus: " + e.getMessage());
             e.printStackTrace();
+        } catch (InvalidInputException | InsufficientMarkovChainException e) {
+            throw new RuntimeException(e);
         }
     }
 }
